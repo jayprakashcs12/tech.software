d3f18f0d1dd09a81ee2654d544db029b1af78c2a
Jay Prakash Singh <jayprakash199221@gmail.com>
2024-05-23 22:40:42 +0530

    Updated code

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 
 const About = () => {
+
+    useEffect(()=> {
+        document.title = "About Us";
+    })
 
     return (
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 
 const Home = () => {
+
+    useEffect(()=> {
+        document.title = "Home";
+    })
 
     return (
 
