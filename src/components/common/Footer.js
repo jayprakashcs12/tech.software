50f54678760558a6cf45a33b0fb44c144fcbef85
Jay Prakash Singh <jayprakash199221@gmail.com>
2024-05-23 23:09:01 +0530

    pro-updated code

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Footer = () => {
 
     return (
 
         <React.Fragment>
-            <h1 className='pro-head'>Footer</h1>
+            <Container className='pro-container'>
+                <h1 className='pro-head'>Footer</h1>
+            </Container>
         </React.Fragment>
     )
 }
